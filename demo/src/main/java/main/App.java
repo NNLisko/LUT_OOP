59866dedf57f326aea643d02ec1488b7def1e947
NNLisko <niko.lausto@student.lut.fi>
2025-01-08 14:53:39 +0200

    java fibonacci

--- demo/src/main/java/main/App.java
+++ demo/src/main/java/main/App.java
@@ -1,13 +1,22 @@
 package main;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
+    static int fibonacci(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        
+        return fibonacci(n - 1) + fibonacci(n - 2);
+        
+    }
+    
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int n = 7;
+        int result = fibonacci(n - 1);
+        System.out.println(result);
     }
 }
+
+
